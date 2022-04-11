346caae8c4075df9d7641bd8771081729faedf59
Harish <48938639+therealharish@users.noreply.github.com>
2022-04-11 13:41:21 +0000

    invert binary tree

--- main.py
+++ main.py
@@ -1,18 +1,1 @@
-l = [2,5,3,7,8,1,9]
-# l=[5,7,4,9,8,10]
-c = []
-s=[]
-i=0;
-for i in range(len(l)):
-  s = l[i+1:]
-  flag=1
-  while(s):
-    if(s[0]>l[i]):
-      c.append(s[0])
-      flag=0
-      break;
-    else:
-      s.pop(0)
-  if(flag):
-    c.append(-1)
-print(c)
+s = 
