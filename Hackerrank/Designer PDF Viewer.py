dadbc911460122c4ec36f6f37eeed519e96f13dd
Harish <48938639+therealharish@users.noreply.github.com>
2022-05-17 11:19:14 +0000

    tree

---
+++ Hackerrank/Designer PDF Viewer.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'designerPdfViewer' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY h
+#  2. STRING word
+#
+
+def designerPdfViewer(h, word):
+  word = word.lower()
+  l=[]
+  for i in word:
+    j = int(ord(i))
+    l.append(h[j-97])
+  m = max(l)
+  area = m*len(word)
+  return area
+      
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    h = list(map(int, input().rstrip().split()))
+
+    word = input()
+
+    result = designerPdfViewer(h, word)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
