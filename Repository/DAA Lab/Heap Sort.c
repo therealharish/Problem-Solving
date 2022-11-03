151dd235c1c16e60536c691342cc5ff6f31dc457
Harish <48938639+therealharish@users.noreply.github.com>
2022-11-03 09:37:01 +0000

    update

---
+++ Repository/DAA Lab/Heap Sort.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define MIN 1
+#define MAX n
+
+void swap(int *a, int *b) {
+  int t = *a;
+  *a = *b;
+  *b = t;
+}
+
+void heapify(int arr[], int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+  
+    if (left < n && arr[left] > arr[largest])
+      largest = left;
+  
+    if (right < n && arr[right] > arr[largest])
+      largest = right;
+    if (largest != i) {
+      swap(&arr[i], &arr[largest]);
+      heapify(arr, n, largest);
+    }
+  }
+
+
+  void heapSort(int arr[], int n) {
+    for (int i = n / 2 - 1; i >= 0; i--)
+      heapify(arr, n, i);
+    for (int i = n - 1; i >= 0; i--) {
+      swap(&arr[0], &arr[i]);
+      heapify(arr, i, 0);
+    }
+  }
+ 
+
+int main() {
+  time_t start = time(NULL);
+  
+  int n;
+  printf("\nEnter size: ");
+  scanf("%d", &n);
+  int *arr = (int *) malloc (n * sizeof(int));
+  for(int i=0; i<n; i++) {
+    arr[i] = (rand() % (MAX - MIN + 1)) + MIN;
+  }
+  printf("\nArray: \n");
+  for(int i=0; i<n; i++) {
+  	printf("%d ", arr[i]);
+  }
+  heapSort(arr,n);
+  printf("\nSorted Array: \n");
+  for(int i=0; i<n; i++) {
+  	printf("%d ", arr[i]);
+  }
+  time_t stop = time(NULL);
+  printf("\nTime taken: %0.2f", difftime(stop,start));
+}
+
